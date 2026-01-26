6d28e8a029990ec6bac0e7417fbe67c45b7e6857
subhansusilo <164128640+subhansusilo@users.noreply.github.com>
2026-01-26 16:38:00 +0700

    Update and rename code-example.py to struktur.py

---
+++ praktikum/week13-tinychain/src/struktur.py
@@ -0,0 +1,21 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, previous_hash, data, timestamp=None):
+        self.index = index
+        self.timestamp = timestamp or time.time()
+        self.data = data
+        self.previous_hash = previous_hash
+        self.nonce = 0
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        value = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash) + str(self.nonce)
+        return hashlib.sha256(value.encode()).hexdigest()
+
+    def mine_block(self, difficulty):
+        while self.hash[:difficulty] != "0" * difficulty:
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+        print(f"Block mined: {self.hash}")
